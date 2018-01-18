d5ba68dae748a6a5393da97b0022d856c190ac94
VictorPullzz <GORPONFFFFFF@yandex.ru>
2018-01-18 19:16:21 +0400

    Finish

--- dev/js/unocompany.js
+++ dev/js/unocompany.js
@@ -7,8 +7,36 @@
 		$sel.body = $("body", $sel.html);
 
 		return {
+			page: {
+				slider: function() {
+					var self = this,
+						$itemSlider = $(".main-slider-item");
 
+					$(".slick-slider").slick({
+						dots: false,
+						arrows: false,
+						infinite: true,
+						autoplay: true,
+						autoplaySpeed: "5000",
+						speed: 700,
+						slidesToShow: 1,
+					});
+
+					$itemSlider.on("mousedown", function() {
+						item = $(this);
+						item.css("cursor", "-webkit-grab");
+					})
+
+					$itemSlider.on("mouseup", function() {
+						item = $(this);
+						item.css("cursor", "pointer");
+					})
+				}
+			}
 		};
 
 	})();
+
+	UNOCOMPANY.page.slider();
+
 })(jQuery);
