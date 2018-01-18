cd1ab6b8da79fd36ca0296b2e84b40f9f54ac850
VictorPullzz <GORPONFFFFFF@yandex.ru>
2018-01-18 15:48:33 +0400

    Start

---
+++ dev/js/unocompany.js
@@ -0,0 +1,14 @@
+(function($) {
+	var UNOCOMPANY = (function() {
+
+		var $sel = {};
+		$sel.window = $(window);
+		$sel.html = $("html");
+		$sel.body = $("body", $sel.html);
+
+		return {
+
+		};
+
+	})();
+})(jQuery);
